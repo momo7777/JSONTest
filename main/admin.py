372dfa94dc81b677d59e30e052fea939fe8be66d
hyejin <hyejinmo0114@gmail.com>
2016-07-18 17:32:42 -0700

    add admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from models import stuffs_sort
 # Register your models here.
+
+class stuffs_sortAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'image')
+
+admin.site.register(stuffs_sort , stuffs_sortAdmin)
+
