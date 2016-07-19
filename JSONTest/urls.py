752304acac34b9c2d2a34122cb80fbc4705b0fbb
hyejin <hyejinmo0114@gmail.com>
2016-07-18 17:22:38 -0700

    add url

--- JSONTest/urls.py
+++ JSONTest/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from main.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', main),
+    url(r'^filtered_userstuff/', filtered_userstuff_data),
 ]
